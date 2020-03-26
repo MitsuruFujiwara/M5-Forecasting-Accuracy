6a9d8c653a8168d7408391774ff451cbe663365a
MitsuruFujiwara <fujiwara52jp@gmail.com>
2020-03-27 07:48:15 +0900

    LB#13 kernel blending

---
+++ src/301_blend_kernel.py
@@ -0,0 +1,30 @@
+
+import pandas as pd
+import numpy as np
+
+from utils import line_notify, submit
+
+#===============================================================================
+# blending kernel predictions
+#===============================================================================
+
+def main():
+    # load predictions
+    sub1 = pd.read_csv('../input/sub_058724.csv',index_col=0)
+    sub2 = pd.read_csv('../input/sub_057330.csv',index_col=0)
+
+    # average
+    sub = 0.5*sub1 + 0.5*sub2
+
+    # reset index
+    sub.reset_index(inplace=True)
+
+    # save csv
+    sub.to_csv(submission_file_name,index=False)
+
+    # submission by API
+    submit(submission_file_name, comment='model301')
+
+if __name__ == '__main__':
+    submission_file_name = '../output/sub_blend.csv'
+    main()
