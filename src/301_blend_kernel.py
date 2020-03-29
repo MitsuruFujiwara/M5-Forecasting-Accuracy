7cd428c4f632a08614e0045dddfc9052ed211e31
MitsuruFujiwara <fujiwara52jp@gmail.com>
2020-03-29 20:41:18 +0900

    LB#18 kernel blending

--- src/301_blend_kernel.py
+++ src/301_blend_kernel.py
@@ -11,11 +11,11 @@ from utils import line_notify, submit
 def main():
     # load predictions
     sub1 = pd.read_csv('../input/sub_058724.csv',index_col=0)
-    sub2 = pd.read_csv('../input/sub_057330.csv',index_col=0)
+#    sub2 = pd.read_csv('../input/sub_057330.csv',index_col=0)
     sub3 = pd.read_csv('../input/sub_054431.csv',index_col=0)
 
     # average
-    sub = 0.3*sub1 + 0.3*sub2 + 0.4*sub3
+    sub = 0.5*sub1 + 0.5*sub3
 
     # reset index
     sub.reset_index(inplace=True)
