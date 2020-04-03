283ed39d8d7d320f85b9167e5794878aa51d9380
MitsuruFujiwara <fujiwara52jp@gmail.com>
2020-04-03 14:27:04 +0900

    LB#20 kernel blending

--- src/301_blend_kernel.py
+++ src/301_blend_kernel.py
@@ -10,13 +10,17 @@ from utils import line_notify, submit
 
 def main():
     # load predictions
-    sub1 = pd.read_csv('../input/sub_058724.csv',index_col=0)
-    sub2 = pd.read_csv('../input/sub_057330.csv',index_col=0)
+#    sub1 = pd.read_csv('../input/sub_058724.csv',index_col=0)
+#    sub2 = pd.read_csv('../input/sub_057330.csv',index_col=0)
     sub3 = pd.read_csv('../input/sub_054431.csv',index_col=0)
     sub4 = pd.read_csv('../input/sub_054218.csv',index_col=0)
+    sub5 = pd.read_csv('../input/sub_053171.csv',index_col=0)
 
     # average
-    sub = 0.2*sub1 + 0.2*sub2 + 0.3*sub3+ 0.3*sub4
+    sub = 0.5*sub3 + 0.5*sub4
+    sub = 0.5*sub + 0.5*sub5
+#    sub = 0.5*sub + 0.5*sub4
+#    sub = 0.5*sub + 0.5*sub5
 
     # reset index
     sub.reset_index(inplace=True)
