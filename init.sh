631386b449d3aea27caadabb08f4d37c7ad03a06
MitsuruFujiwara <fujiwara52jp@gmail.com>
2020-04-07 22:21:08 +0900

    add logger

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 mkdir feats
 mkdir input
 mkdir output
+mkdir log
 cd input
 kaggle competitions download -c m5-forecasting-accuracy
 unzip '*.zip'
--- src/run.sh
+++ src/run.sh
@@ -3,8 +3,8 @@ cd ../feats
 rm *.feather
 rm *.pkl
 cd ../src
-python 001_sales.py
-python 002_calendar.py
-python 003_sell_prices.py
-python 101_aggregation.py
-python 202_train_lgbm_holdout.py
+python 001_sales.py > ../log/001_sales.py.txt
+python 002_calendar.py > ../log/002_calendar.py.txt
+python 003_sell_prices.py > ../log/003_sell_prices.py.txt
+python 101_aggregation.py > ../log/101_aggregation.txt
+python 202_train_lgbm_holdout.py > ../log/202_train_lgbm_holdout.py.txt
