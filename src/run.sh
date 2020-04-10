1b6c4802957d3c4baf438520aee3f52b50f7659c
MitsuruFujiwara <fujiwara52jp@gmail.com>
2020-04-10 11:06:49 +0900

    fixed run.sh

--- src/run.sh
+++ src/run.sh
@@ -3,8 +3,8 @@ cd ../feats
 rm *.feather
 rm *.pkl
 cd ../src
-python 001_sales.py > ../log/001_sales.py.txt
-python 002_calendar.py > ../log/002_calendar.py.txt
-python 003_sell_prices.py > ../log/003_sell_prices.py.txt
-python 101_aggregation.py > ../log/101_aggregation.txt
-python 202_train_lgbm_holdout.py > ../log/202_train_lgbm_holdout.py.txt
+echo python 001_sales.py > ../log/001_sales.py.txt
+echo python 002_calendar.py > ../log/002_calendar.py.txt
+echo python 003_sell_prices.py > ../log/003_sell_prices.py.txt
+echo python 101_aggregation.py > ../log/101_aggregation.txt
+echo python 202_train_lgbm_holdout.py > ../log/202_train_lgbm_holdout.py.txt
