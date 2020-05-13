2d505ff9110e87320ef4246eed3cb87508f21e8c
MitsuruFujiwara <fujiwara52jp@gmail.com>
2020-05-13 13:01:07 +0900

    fixed

--- src/run.sh
+++ src/run.sh
@@ -2,11 +2,11 @@
 cd ../feats
 rm *.feather
 rm *.pkl
-rm /sales/*.pkl
+rm sales/*.pkl
 cd ../src
 python 001_sales.py
 python 002_calendar.py
 python 003_sell_prices.py
 python 101_aggregation.py
-python 203_train_lgbm_all_data.py
-#python 204_train_lgbm_custom_cv.py
+#python 203_train_lgbm_all_data.py
+python 204_train_lgbm_custom_cv.py
