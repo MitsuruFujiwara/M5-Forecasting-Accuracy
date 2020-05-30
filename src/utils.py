6fab193d46a3676e858a3ea56528ffbebf05c4d7
MitsuruFujiwara <fujiwara52jp@gmail.com>
2020-05-30 20:10:00 +0900

    add group k-fold

--- src/utils.py
+++ src/utils.py
@@ -189,14 +189,14 @@ def stop_instance():
 def custom_asymmetric_train(y_pred, y_true):
     y_true = y_true.get_label()
     residual = (y_true - y_pred).astype("float")
-    grad = np.where(residual < 0, -2 * residual, -2 * residual * 1.2)
-    hess = np.where(residual < 0, 2, 2 * 1.2)
+    grad = np.where(residual < 0, -2 * residual, -2 * residual * 1.15)
+    hess = np.where(residual < 0, 2, 2 * 1.15)
     return grad, hess
 
 def custom_asymmetric_valid(y_pred, y_true):
     y_true = y_true.get_label()
     residual = (y_true - y_pred).astype("float")
-    loss = np.where(residual < 0, (residual ** 2) , (residual ** 2) * 1.2)
+    loss = np.where(residual < 0, (residual ** 2) , (residual ** 2) * 1.15)
     return "custom_asymmetric_eval", np.mean(loss), False
 
 # custom time series splitter
