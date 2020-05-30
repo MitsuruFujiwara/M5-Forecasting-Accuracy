1bddaa5ce92827412ce341103b6d13d6fd9ccffb
MitsuruFujiwara <fujiwara52jp@gmail.com>
2020-05-30 14:59:22 +0900

    update custom objective parameters: 1.15 -> 1.2

--- src/utils_lag.py
+++ src/utils_lag.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 # utils for lag features
 #==============================================================================
 
-# 28days lag
+# helper for making lag features
 def make_lags(df,days=28):
     # lag features
     df_grouped = df[['id','demand']].groupby(['id'])['demand']
@@ -19,6 +19,29 @@ def make_lags(df,days=28):
     print('Add rolling aggs...')
     for i in tqdm([7,14,21,28]):
         df[f'demand_rolling_mean_{days}_{i}'] = df_grouped.transform(lambda x: x.shift(days).rolling(i).mean())
+        df[f'demand_rolling_std_{days}_{i}'] = df_grouped.transform(lambda x: x.shift(days).rolling(i).std())
+        df[f'demand_rolling_max_{days}_{i}'] = df_grouped.transform(lambda x: x.shift(days).rolling(i).max())
+        df[f'demand_rolling_min_{days}_{i}'] = df_grouped.transform(lambda x: x.shift(days).rolling(i).min())
+        df[f'demand_rolling_skew_{days}_{i}'] = df_grouped.transform(lambda x: x.shift(days).rolling(i).skew())
+        df[f'demand_rolling_kurt_{days}_{i}'] = df_grouped.transform(lambda x: x.shift(days).rolling(i).kurt())
+
+    del df_grouped
+    gc.collect()
+
+    return df
+
+# TODO: # helper for making lag features with ewm
+def make_lags_ewm(df,days=28):
+    # lag features
+    df_grouped = df[['id','demand']].groupby(['id'])['demand']
+
+    print('Add lag features...')
+    for i in tqdm([0,7,14,21,28]):
+        df[f'demand_lag_{days}_{i}'] = df_grouped.shift(days+i)
+
+    print('Add rolling aggs...')
+    for i, alpha in tqdm(zip([7,14,21,28],[0.1,0.075,0.05,0.025])):
+        df[f'demand_rolling_mean_{days}_{i}'] = df_grouped.transform(lambda x: x.shift(days).rolling(i).ewm(alpha=alpha))
         df[f'demand_rolling_std_{days}_{i}'] = df_grouped.transform(lambda x: x.shift(days).rolling(i).std())
         df[f'demand_rolling_max_{days}_{i}'] = df_grouped.transform(lambda x: x.shift(days).rolling(i).max())
         df[f'demand_rolling_min_{days}_{i}'] = df_grouped.transform(lambda x: x.shift(days).rolling(i).min())
