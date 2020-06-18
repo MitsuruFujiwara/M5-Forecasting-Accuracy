de4cfd58c3e9af128eb712098ace16dfaa271fab
MitsuruFujiwara <fujiwara52jp@gmail.com>
2020-06-18 17:27:38 +0900

    fixed & update 501_blend.py

--- src/run.sh
+++ src/run.sh
@@ -26,9 +26,9 @@ python 203_cv_lgbm_14days.py
 python 204_cv_lgbm_7days.py
 #python 205_cv_lgbm_diff.py
 #python 206_cv_lgbm_group_k_fold.py
-python 207_cv_lgbm_foods.py
-python 208_cv_lgbm_household.py
-python 209_cv_lgbm_hobbies.py
+#python 207_cv_lgbm_foods.py
+#python 208_cv_lgbm_household.py
+#python 209_cv_lgbm_hobbies.py
 python 210_cv_lgbm_group_k_fold_foods.py
 python 211_cv_lgbm_group_k_fold_household.py
 python 212_cv_lgbm_group_k_fold_hobbies.py
@@ -36,8 +36,8 @@ python 213_cv_lgbm_group_k_fold_28days.py
 python 214_cv_lgbm_group_k_fold_21days.py
 python 215_cv_lgbm_group_k_fold_14days.py
 python 216_cv_lgbm_group_k_fold_7days.py
-python 217_cv_lgbm_weekday.py
-python 218_cv_lgbm_holiday.py
+#python 217_cv_lgbm_weekday.py
+#python 218_cv_lgbm_holiday.py
 python 219_cv_lgbm_group_k_fold_weekday.py
 python 220_cv_lgbm_group_k_fold_holiday.py
 
@@ -46,11 +46,11 @@ python 302_train_lgbm_21days.py
 python 303_train_lgbm_14days.py
 python 304_train_lgbm_7days.py
 #python 305_train_lgbm_diff.py
-python 306_train_lgbm_foods.py
-python 307_train_lgbm_household.py
-python 308_train_lgbm_hobbies.py
-python 309_train_lgbm_weekday.py
-python 310_train_lgbm_holiday.py
+#python 306_train_lgbm_foods.py
+#python 307_train_lgbm_household.py
+#python 308_train_lgbm_hobbies.py
+#python 309_train_lgbm_weekday.py
+#python 310_train_lgbm_holiday.py
 
 python 401_predict_28days.py
 python 404_predict_cat_id.py
